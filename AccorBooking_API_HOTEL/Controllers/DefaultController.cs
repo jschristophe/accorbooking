7d170f54ad295446fcd0ee4aaf84f6dc9ec12402
Christopher JOSEPH <Christopher.JOSEPH@gmail.com>
2019-10-13 17:21:47 +0200

    --

---
+++ AccorBooking_API_HOTEL/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccorBooking_API_HOTEL.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        // GET 
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var result = "API_CATOLOG";           
+
+            return result;
+            //return new string[] { "value1", "value2" };
+        }
+
+       
+    }
+}
